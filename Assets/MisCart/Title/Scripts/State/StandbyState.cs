34d46c889ecfb6511c9a6d5008cf19925febc561
kuro-me <q.summer0812.suika@gmail.com>
2018-08-29 00:23:58 +0900

    初期化用のステートとスプラッシュスクリーンのステートを追加

---
+++ Assets/MisCart/Title/Scripts/State/StandbyState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using BehaviourMachine;
+
+namespace Title
+{
+    [AddComponentMenu("MisCart/Title/StandbyState")]
+    public class StandbyState : StateBehaviour
+    {
+
+        /// <summary>
+        /// UIを一度全て非表示にさせる
+        /// </summary>
+        void Start ()
+        {
+    		TitleManager.UI.SplashScreen.SetActive(false);
+    		TitleManager.UI.Loading.SetActive(false);
+    		TitleManager.UI.SlideShow.SetActive(false);
+    		TitleManager.UI.Start.SetActive(false);
+    		TitleManager.Front.Camera.gameObject.SetActive(false);
+    		TitleManager.Front.SetActive(false);
+            SendEvent("EndAction");
+        }
+
+        void Update () {
+
+        }
+    }
+}
