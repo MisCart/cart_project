57206cbae7026ffd5b6119a505fd2067e3772174
kuro-me <q.summer0812.suika@gmail.com>
2018-08-31 21:22:21 +0900

    スプラッシュスクリーンをFSMで管理

---
+++ Assets/MisCart/Title/Scripts/State/SplashScreenState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BehaviourMachine;
+using MisCart;
+
+namespace Title
+{
+	[AddComponentMenu("MisCart/Title/SplashScreenState")]
+	public class SplashScreenState : StateBehaviour
+	{
+		BoolVar isScreen;
+		void Start ()
+		{
+			isScreen = blackboard.GetBoolVar("IsSplashScreen");
+			TitleManager.UI.SplashScreen.SetActive(true);
+
+			//スプラッシュスクリーン
+			var screen = TitleManager.UI.SplashScreen.Screen(() => isScreen.Value = false);
+			StartCoroutine(screen);
+		}
+
+		void Update()
+		{
+			if(!isScreen){
+				SendEvent("EndAction");
+			}
+		}
+
+		private void OnDisable() {
+			TitleManager.UI.SplashScreen.SetActive(false);
+		}
+	}
+}
