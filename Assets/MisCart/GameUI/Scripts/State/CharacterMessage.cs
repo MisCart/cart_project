13687dd31673f8634ef008c06d03383993f0e25d
kuro-me <q.summer0812.suika@gmail.com>
2018-10-21 22:25:25 +0900

    キャラクターの台詞を追加

---
+++ Assets/MisCart/GameUI/Scripts/State/CharacterMessage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameUI
+{
+	public class CharacterMessage
+	{
+        public string misChanBlueWin = "楽しいレースだったよ！";
+        public string misChanBlueLose = "次は負けてられないぞ...！";
+
+        public string misChanGreenWin = "皆様の応援、感謝しております";
+        public string misChanGreenLose = "実力が足りなかったようですね...";
+
+        public string misChanLightBlueWin = "わわわ私が勝つなんて";
+        public string misChanLightBlueLose = "実力が足りなかったようですね...";
+
+        public string misChanPinkWin = "ラッキー私の勝ち！";
+        public string misChanPinkLose = "こんな結果ありえないんですケド？";
+
+        public string misChanRedWin = "熱いバトルだったぜ！";
+        public string misChanRedLose = "オイオイこんなのありかよ！？";
+
+        public string misChanYellowWin = "こんなのボクなら楽勝！";
+        public string misChanYellowLose = "みんな強すぎるよ！？";
+	}
+}
---
+++ Assets/MisCart/GameUI/Scripts/State/ResultAnimationState.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using BehaviourMachine;
+
+namespace GameUI
+{
+    [AddComponentMenu("GameUI/ResultAnimationState")]
+    public class ResultAnimationState : GameUIStateBehaviour
+    {
+
+        void Start ()
+        {
+        }
+        public override void OnClick(){}
+		public override void OnBack(){}
+    }
+}
