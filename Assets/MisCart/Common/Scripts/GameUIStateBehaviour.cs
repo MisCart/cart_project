902c2e7a6af7a8457fa85a2ebec9660e4c9b5655
kuro-me <q.summer0812.suika@gmail.com>
2018-10-21 11:35:17 +0900

    終了演出をresultシーンではなくgameシーンで行うように変更

--- Assets/MisCart/Common/Scripts/GameUIStateBehaviour.cs
+++ Assets/MisCart/Common/Scripts/GameUIStateBehaviour.cs
@@ -8,7 +8,7 @@ using System.Collections;
 
 namespace BehaviourMachine {
 
-    public abstract class ResultStateBehaviour : InternalStateBehaviour {
+    public abstract class GameUIStateBehaviour : InternalStateBehaviour {
         public abstract void OnClick();
         public abstract void OnBack();
     }
