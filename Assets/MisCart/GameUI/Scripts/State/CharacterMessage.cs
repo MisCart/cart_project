a5a18b2d12927ab76d5caeda3e912a1b6c1c6031
kuro-me <q.summer0812.suika@gmail.com>
2018-11-07 19:23:09 +0900

    台詞修正

--- Assets/MisCart/GameUI/Scripts/State/CharacterMessage.cs
+++ Assets/MisCart/GameUI/Scripts/State/CharacterMessage.cs
@@ -9,10 +9,10 @@ namespace GameUI
         public static string misChanBlueLose = "次は負けてられないぞ...！";
 
         public static string misChanGreenWin = "皆様の応援、感謝しております";
-        public static string misChanGreenLose = "実力が足りなかったようですね...";
+        public static string misChanGreenLose = "私の実力が足りなかったようですね...";
 
-        public static string misChanLightBlueWin = "わわわ私が勝つなんて";
-        public static string misChanLightBlueLose = "実力が足りなかったようですね...";
+        public static string misChanLightBlueWin = "わわわ、私が勝つなんて...";
+        public static string misChanLightBlueLose = "どうせ私なんか...";
 
         public static string misChanPinkWin = "ラッキー私の勝ち！";
         public static string misChanPinkLose = "こんな結果ありえないんですケド？";
